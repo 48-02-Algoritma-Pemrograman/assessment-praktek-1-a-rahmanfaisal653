9391da7bea4689f455ccdebac0de0e32ba188232
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-10-24 11:27:23 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+        double honorLembur = 0;
+        int jamLembur = 0;
+        jamLembur = input.nextInt();
+
+        if(jamLembur <= 16 ){
+            honorLembur = 10000 * jamLembur;
+        }
+        else if (jamLembur > 16) {
+            honorLembur = (160000) + (15000 * (jamLembur - 16));
+        } 
+        else {
+            
+        }
+        System.out.println("Total jam lembur : " + jamLembur);
+        System.out.println("Honor Lembur : " + honorLembur);
+
+
+        input.close();
     }
 }
