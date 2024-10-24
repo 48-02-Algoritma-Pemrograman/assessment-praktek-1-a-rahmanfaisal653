b2f4b3a3830269e81e0c12a7372bc41136403128
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-10-24 12:37:17 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -20,44 +20,7 @@ public class Nomor1{
         System.out.println("Total jam lembur : " + jamLembur);
         System.out.println("Honor Lembur : " + honorLembur);
 
-        //no 2
-
-        System.out.println("Masukan nama driver : ");
-        String driver = input.next();
-
-        System.out.println("Masukan lama trip : ");
-        int km = input.nextInt();
-
-        System.out.println("Masukan Input lagi? : ");
-        String inputLagi = input.next();
-
-        for (int i = 0; i <= 1; i++) {
-            switch (inputLagi) {
-                case "y":
-
-                    System.out.println("Masukan nama driver : ");
-                    driver = input.next();
-
-                    System.out.println("Masukan lama trip : ");
-                    km = input.nextInt();
-
-                    System.out.println("Masukan Input lagi? : ");
-                    inputLagi = input.next();
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        int totalKm =+ km;
-        System.out.println("Total KM : " + totalKm);
-        double totalOmset = km * 13000;
-        System.out.print("Total Omset : " + totalOmset);
-
         
-    
-
 
         input.close();
     }
