c1e9d4f4b43f8090be0867cbc33db31101d0a079
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2024-10-24 12:39:29 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,42 @@
-public class Nomor2{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukan nama driver : ");
+        String driver = input.next();
+
+        System.out.println("Masukan lama trip : ");
+        int km = input.nextInt();
+
+        System.out.println("Masukan Input lagi? : ");
+        String inputLagi = input.next();
+
+        for (int i = 0; i <= 1; i++) {
+            switch (inputLagi) {
+                case "y":
+
+                    System.out.println("Masukan nama driver : ");
+                    driver = input.next();
+
+                    System.out.println("Masukan lama trip : ");
+                    km = input.nextInt();
+
+                    System.out.println("Masukan Input lagi? : ");
+                    inputLagi = input.next();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        int totalKm =+ km;
+        System.out.println("Total KM : " + totalKm);
+        double totalOmset = km * 13000;
+        System.out.print("Total Omset : " + totalOmset);
+
+        input.close();
     }
 }
